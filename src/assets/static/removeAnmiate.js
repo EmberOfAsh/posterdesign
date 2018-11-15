9b1564281725f737cfa8eaf1f31710598c27f5bb
597344951 <597344951@qq.com>
2018-11-15 10:47:41 +0800

    延时1.2s后移除加载动画

--- src/assets/static/removeAnmiate.js
+++ src/assets/static/removeAnmiate.js
@@ -1,19 +1,18 @@
 /**
  * fix: html静态化后, 移除加载动画class
  *  */
-window.onload = function () {
-  setTimeout(() => {
-    removeAnmiate()
-  }, 1000)
 
-  function removeAnmiate () {
-    let inAnmiates = document.querySelectorAll('.v-enter')
-    inAnmiates.forEach(el => {
-      let css = el.getAttribute('class')
-      let idx = css.indexOf('v-enter')
-      let nc = css.substring(0, idx)
-      console.debug('截取加载动画class:', css, ' -> ', nc)
-      el.setAttribute('class', nc)
-    })
-  }
+setTimeout(() => {
+  removeAnmiate()
+}, 1200)
+
+function removeAnmiate () {
+  let inAnmiates = document.querySelectorAll('.v-enter')
+  inAnmiates.forEach(el => {
+    let css = el.getAttribute('class')
+    let idx = css.indexOf('v-enter')
+    let nc = css.substring(0, idx)
+    console.debug('截取加载动画class:', css, ' -> ', nc)
+    el.setAttribute('class', nc)
+  })
 }
